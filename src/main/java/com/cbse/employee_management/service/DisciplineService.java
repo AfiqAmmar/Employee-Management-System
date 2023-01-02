c142647ae7a87c7d8512080e668ba38f4f6c6cc8
Humaira' AS <whomyras00@gmail.com>
2023-01-02 20:28:59 +0800

    Add discipline service

---
+++ src/main/java/com/cbse/employee_management/service/DisciplineService.java
@@ -0,0 +1,12 @@
+package com.cbse.employee_management.service;
+
+import java.util.List;
+
+import com.cbse.employee_management.model.Discipline;
+
+public interface DisciplineService {
+	List<Discipline> getAllDisciplines();
+	void saveDiscipline(Discipline discipline);
+	List<Discipline> getDisciplineByEmployeeId(long id);
+	// void deleteDisciplineById(long id);
+}
