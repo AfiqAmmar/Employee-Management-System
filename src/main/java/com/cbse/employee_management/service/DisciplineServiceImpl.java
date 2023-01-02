b272552528842727f5e692ae7ea787b6ed7f9c29
Humaira' AS <whomyras00@gmail.com>
2023-01-02 20:30:17 +0800

    Add discipline service impl

---
+++ src/main/java/com/cbse/employee_management/service/DisciplineServiceImpl.java
@@ -0,0 +1,32 @@
+package com.cbse.employee_management.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cbse.employee_management.model.Discipline;
+import com.cbse.employee_management.repository.DisciplineRepository;
+
+@Service
+public class DisciplineServiceImpl implements DisciplineService{
+    @Autowired
+	private DisciplineRepository disciplineRepository;
+
+	@Override
+	public List<Discipline> getAllDisciplines() {
+		return disciplineRepository.findAll();
+	}
+
+	@Override
+	public void saveDiscipline(Discipline discipline) {
+		this.disciplineRepository.save(discipline);
+		
+	}
+
+	@Override
+	public List<Discipline> getDisciplineByEmployeeId(long id) {
+		return disciplineRepository.findByEmployeeId(id);
+	}
+}
